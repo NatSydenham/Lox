a04a67202d6613a2215f96e81402f769d4f9acc1
Nathanial Sydenham <nathanial.sydenham@outlook.com>
2023-12-27 13:29:47 +0000

    setup tokens and types, basic input

--- Lox/Program.cs
+++ Lox/Program.cs
@@ -1,10 +1,74 @@
-﻿namespace Lox
+﻿using System.Text;
+
+namespace Lox
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private const int ARGS_LENGTH = 1;
+        private const int INCORRECT_USAGE = 64;
+
+        private static bool hadError = false;
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            if (args.Length > ARGS_LENGTH)
+            {
+                Console.WriteLine("Usage: dotnet lox.dll [script]");
+                Environment.Exit(INCORRECT_USAGE);
+            }
+            else if (args.Length == ARGS_LENGTH)
+            {
+                RunFile(args[0]);
+            }
+            else
+            {
+                RunPrompt();
+            }
+        }
+
+        private static void RunFile(string file)
+        {
+            var bytes = File.ReadAllBytes(file);
+            Run(Encoding.UTF8.GetString(bytes));
+            if (hadError)
+            {
+                Environment.Exit(INCORRECT_USAGE);
+            }
+        }
+
+        private static void RunPrompt()
+        {
+            using (var reader = new StreamReader(Console.OpenStandardInput()))
+            {
+                for (; ; )
+                {
+                    Console.WriteLine("> ");
+                    var line = reader.ReadLine();
+
+                    if (line == null)
+                    {
+                        break;
+                    }
+
+                    Run(line);
+                    hadError = false;
+                }
+            }
+        }
+
+        private static void Run(string source)
+        {
+            Console.WriteLine(source);
+        }
+
+        public static void Error(int line, string message)
+        {
+            Report(line, "", message);
+        }
+
+        private static void Report(int line, string where, string message)
+        {
+            Console.Error.WriteLine($"[line {line}] Error {where}: {message}");
+            hadError = true;
         }
     }
 }
---
+++ Lox/Token.cs
@@ -0,0 +1,25 @@
+﻿namespace Lox
+{
+    internal class Token
+    {
+        private readonly TokenType type;
+        private readonly string lexeme;
+        private readonly object literal;
+        private readonly int line;
+
+
+        public Token(TokenType type, string lexeme, object literal, int line)
+        {
+            this.type = type;
+            this.lexeme = lexeme;
+            this.literal = literal;
+            this.line = line;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{type} {lexeme} {literal}";
+        }
+    }
+}
